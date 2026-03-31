20646b5c490d4cf98b2000310eb13f246fa0a3f5
Hannah <hannah.xia@ucalgary.ca>
2026-03-31 12:14:44 -0600

    remove bugs from ProfilePage

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { useAuth } from '../context/AuthContext';
-import { updateUser, changePassword } from '../api/users';
-import { getUserBookings } from '../api/bookings';
+//import React, { useState, useEffect } from 'react';
+//import { useAuth } from '../context/AuthContext';
+//import { updateUser, changePassword } from '../api/users';
+//import { getUserBookings } from '../api/bookings';
 // import BookingCard from '../components/bookings/BookingCard';
 
 const ProfilePage = () => {
-  const { user, setUser, logout } = useAuth();
-  const [activeTab, setActiveTab] = useState("profile"); //'profile', 'bookings', 'security'
-  const [isEditing, setIsEditing] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState({ type: "", text: "" });
-  const [bookings, setBookings] = useState([]);
-  const [bookingsLoading, setBookingsLoading] = useState(false);
+  // const { user, setUser, logout } = useAuth();
+  // const [activeTab, setActiveTab] = useState("profile"); //'profile', 'bookings', 'security'
+  // const [isEditing, setIsEditing] = useState(false);
+  // const [loading, setLoading] = useState(false);
+  // const [message, setMessage] = useState({ type: "", text: "" });
+  // const [bookings, setBookings] = useState([]);
+  // const [bookingsLoading, setBookingsLoading] = useState(false);
 
-  //form states --> for standard profile changes
-  const [profileForm, setProfileForm] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    passportNumber: "",
-    nationality: "",
-    dateOfBirth: ""
-  });
+  // //form states --> for standard profile changes
+  // const [profileForm, setProfileForm] = useState({
+  //   name: "",
+  //   email: "",
+  //   phone: "",
+  //   passportNumber: "",
+  //   nationality: "",
+  //   dateOfBirth: ""
+  // });
 
-  //for changing password --> set a seperate form
-  const [passwordForm, setPasswordForm] = useState({
-    currentPassword: "",
-    newPassword: "",
-    confirmPassword: ""
-  });
+  // //for changing password --> set a seperate form
+  // const [passwordForm, setPasswordForm] = useState({
+  //   currentPassword: "",
+  //   newPassword: "",
+  //   confirmPassword: ""
+  // });
 
 
 
